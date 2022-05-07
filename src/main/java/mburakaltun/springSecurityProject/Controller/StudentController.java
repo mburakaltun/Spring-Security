6b44c5cf358c3c695e1e91c44fac78c92e198489
MUHAMMED BURAK ALTUN <MALTUN15@ku.edu.tr>
2022-05-07 18:08:20 +0300

    Basic Auth Configuration

---
+++ src/main/java/mburakaltun/springSecurityProject/Controller/StudentController.java
@@ -0,0 +1,29 @@
+package mburakaltun.springSecurityProject.Controller;
+
+import mburakaltun.springSecurityProject.Model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Burak Altun"),
+            new Student(2, "Begum Altun")
+    );
+
+    @GetMapping(path = "{studentId}")
+    Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist."));
+    }
+}
